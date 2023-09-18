2906ec186923140a666cdb649d03aeae268afd02
GidyMane <muragegideon2000@gmail.com>
2023-09-18 13:29:00 -0700

    Update strings

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Gideon Murage';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting);
